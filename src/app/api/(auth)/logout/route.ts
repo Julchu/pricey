6692752b09ee5d4435787827d0460cefc25f71aa
julchu <jchumtl@gmail.com>
2025-06-03 03:28:56 -0400

    Fixing issues from auto-formatting

--- src/app/api/(auth)/logout/route.ts
+++ src/app/api/(auth)/logout/route.ts
@@ -15,26 +15,26 @@ export async function POST() {
           },
         },
       );
-      
+
       const { success, data } = await logoutResponse.json();
 
       const setCookie = logoutResponse.headers.get("set-cookie");
-      
+
       if (success && setCookie)
         return new Response(JSON.stringify({ userInfo: data }), {
           status: 200,
           headers: { "Set-Cookie": setCookie },
         });
     }
-    
+
     const clearCookiesHeader = [
       "pricey_access_token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax",
-      "pricey_refresh_token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax,
+      "pricey_refresh_token=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax",
     ];
-    
+
     return new Response(JSON.stringify({ userInfo: null }), {
       status: 200,
-      headers: { "Set-Cookie": clearCookiesHeader.join(", ") ,
+      headers: { "Set-Cookie": clearCookiesHeader.join(", ") },
     });
   } catch (error) {
     return new Response(`Logout error: ${error}`, {
