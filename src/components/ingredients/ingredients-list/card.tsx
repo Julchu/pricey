3b0a25a64b6cfc8869169846a6e2b54477c1308e
julchu <jchumtl@gmail.com>
2025-06-23 02:13:56 -0400

    Porting over useDebouncedState from old Pricey

--- src/components/ingredients/ingredients-list/card.tsx
+++ src/components/ingredients/ingredients-list/card.tsx
@@ -1,7 +1,10 @@
 "use client";
 // TODO: similar ingredient card to Ingredients card
 import { Ingredient } from "@/utils/interfaces";
-import { ingredientSetValue } from "@/providers/ingredient-form-provider";
+import {
+  ingredientControl,
+  ingredientSetValue,
+} from "@/providers/ingredient-form-provider";
 import {
   calcIndividualPrice,
   formatCurrency,
@@ -10,11 +13,25 @@ import {
 } from "@/utils/text-formatters";
 import { useUserStore } from "@/stores/user-store";
 import { useShallow } from "zustand/react/shallow";
+import { useWatch } from "react-hook-form";
+import { useDebouncedState } from "@/app/hooks/use-debounced-state";
+import { useEffect } from "react";
 
 export const Card = ({ name, price, quantity, unit, capacity }: Ingredient) => {
   const [mass, liquidVolume] = useUserStore(
     useShallow(({ mass, liquidVolume }) => [mass, liquidVolume]),
   );
+
+  const [searchName] = useWatch({
+    name: ["name"],
+    control: ingredientControl,
+  });
+
+  const debouncedSearchName = useDebouncedState(searchName, 500);
+
+  useEffect(() => {
+    console.log("debouncedSearchName:", debouncedSearchName);
+  }, [debouncedSearchName]);
 
   const userUnits = {
     mass,
@@ -49,11 +66,11 @@ export const Card = ({ name, price, quantity, unit, capacity }: Ingredient) => {
   return (
     <div
       className={
-        "mb-4 w-full cursor-pointer justify-center overflow-hidden rounded-md bg-blue-500 p-4 text-center text-xl break-words"
+        "mb-4 w-full cursor-pointer justify-center overflow-hidden rounded-md bg-blue-500 p-4 text-center text-lg break-words"
       }
       onClick={cardClickHandler}
     >
-      <div className={"text-2xl font-bold capitalize"}>{name}</div>
+      <div className={"text-xl font-bold capitalize"}>{name}</div>
       <div>{`${formattedPricePerMeasurement}/${formattedUnit}`}</div>
       <div>{`${formattedPricePerItem}`}</div>
     </div>
