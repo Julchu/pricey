a020fa4d67a75fb517b0b1aa358f338dc08da69e
julchu <jchumtl@gmail.com>
2025-07-26 14:04:33 -0400

    Fixing ReactScan issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { geistMono, montserrat } from "@/components/fonts";
 import "./globals.css";
 import { Header } from "@/components/header/header";
-import { ReactScan } from "@/components/react-scan";
 import { PropsWithChildren } from "react";
 import { Providers } from "@/providers/providers";
 
@@ -23,7 +22,6 @@ export default async function Layout({ children }: PropsWithChildren) {
       <body
         className={`${montserrat.className} ${geistMono.variable} antialiased`}
       >
-        <ReactScan />
         <main
           className={"container mx-auto flex h-dvh w-screen flex-col gap-4 p-4"}
         >
