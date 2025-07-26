f2a4df2b5b51b755ee2566c2794d080c8cefb7da
julchu <jchumtl@gmail.com>
2025-07-26 14:05:29 -0400

    Updating new grocery list form

--- src/components/groceries/new-grocery-list-form.tsx
+++ src/components/groceries/new-grocery-list-form.tsx
@@ -2,11 +2,16 @@
 import { FormProvider, useForm } from "react-hook-form";
 import { GroceryListFormData, UnitType } from "@/utils/interfaces";
 import { useLens } from "@hookform/lenses";
-import { AccordionContent, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  AccordionContent,
+  AccordionHeader,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Input } from "@/components/ingredients/calculator/inputs";
 import { IngredientArrayForm } from "@/components/groceries/ingredient-array-form";
 import * as React from "react";
-// TODO: test if can remove * as React
+import { useEffect } from "react";
+import { SaveCartIcon } from "@/components/icons/cart/save-cart-icon";
 
 export const NewGroceryListForm = () => {
   const methods = useForm<GroceryListFormData>({
@@ -15,8 +20,8 @@ export const NewGroceryListForm = () => {
       ingredients: [
         {
           name: "",
-          price: 0,
-          capacity: 0,
+          price: "" as unknown as number,
+          capacity: "" as unknown as number,
           unit: "" as UnitType,
         },
       ],
@@ -24,27 +29,43 @@ export const NewGroceryListForm = () => {
     },
   });
 
-  const { handleSubmit, control, register } = methods;
+  const {
+    handleSubmit,
+    control,
+    register,
+    setFocus,
+    formState: { isDirty },
+  } = methods;
+
+  useEffect(() => {
+    setFocus("name");
+  }, [setFocus]);
 
   const lens = useLens({ control });
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={handleSubmit((values) => console.log(values))}>
-        <AccordionTrigger>
+      <form>
+        <AccordionHeader className={"flex h-full w-full flex-row gap-4 pl-4"}>
           <Input
-            className={"w-full"}
+            className={"w-full focus:outline-none"}
             autoComplete={"name"}
-            placeholder={"Costco Run"}
+            placeholder={"Enter new grocery list name "}
             id={"name"}
             type={"text"}
             {...register("name")}
           />
-        </AccordionTrigger>
+          <div
+            onClick={handleSubmit((values) => console.log(values))}
+            className={"flex items-center"}
+          >
+            {isDirty ? <SaveCartIcon /> : null}
+          </div>
+          <AccordionTrigger />
+        </AccordionHeader>
 
-        <AccordionContent className={"h-full w-full"}>
+        <AccordionContent>
           <IngredientArrayForm lens={lens.focus("ingredients")} />
-          <input type="submit" />
         </AccordionContent>
       </form>
     </FormProvider>
