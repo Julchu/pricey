8a20e5d80c492a629e912f9e68d387941fcfa03f
julchu <jchumtl@gmail.com>
2025-07-29 21:57:10 -0400

    Replacing ReactScan component with script, using window.location.href Login component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { geistMono, montserrat } from "@/components/fonts";
 import "./globals.css";
 import { Header } from "@/components/header/header";
-import { ReactScan } from "@/components/react-scan";
 import { PropsWithChildren } from "react";
 import { Providers } from "@/providers/providers";
 
@@ -14,10 +13,15 @@ export const metadata: Metadata = {
 export default async function Layout({ children }: PropsWithChildren) {
   return (
     <html lang={"en"}>
+      <head>
+        <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        />
+      </head>
       <body
         className={`${montserrat.className} ${geistMono.variable} antialiased`}
       >
-        <ReactScan />
         <main
           className={"container mx-auto flex h-dvh w-screen flex-col gap-4 p-4"}
         >
