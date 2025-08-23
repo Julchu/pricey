46609528387e1cf06eacb660e985fbf81d2f3175
Eugene <hoihinsauce@gmail.com>
2025-08-23 16:11:45 -0700

    Initial about page with profiles and test info

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: { browserDebugInfoInTerminal: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "en.wikipedia.org",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
