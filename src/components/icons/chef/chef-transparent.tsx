50d218276166fadbd9ec7fa286f62bca81400eaa
julchu <jchumtl@gmail.com>
2026-05-10 11:32:13 -0400

    Fixing icon

--- src/components/icons/chef/chef-transparent.tsx
+++ src/components/icons/chef/chef-transparent.tsx
@@ -12,10 +12,9 @@ export const ChefTransparent = ({ ...props }: SVGProps<SVGSVGElement>) => {
     >
       <path
         fill="none"
-        // stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
         d="M12 3c1.918 0 3.52 1.35 3.91 3.151A4 4 0 0 1 18 13.874V21H6v-7.126a4 4 0 1 1 2.092-7.723A4 4 0 0 1 12 3M6.161 17.009L18 17"
       />
     </svg>
