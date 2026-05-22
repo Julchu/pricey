10626b8543414b60e9a8183c0a30709ff71a2443
julchu <jchumtl@gmail.com>
2026-05-21 21:34:17 -0400

    Adding TODO

--- src/app/groceries/page.tsx
+++ src/app/groceries/page.tsx
@@ -1,6 +1,9 @@
 import { GroceryListAccordion } from "@/components/groceries/grocery-list-accordion";
 
 const Groceries = async () => {
+  // TODO: current list setter; different from new list
+  // Current list toggle (only 1 at a time) will be a checklist
+  // TODO: rename checklists to existing/previous grocery lists
   return (
     <div className="flex w-full flex-col overflow-scroll rounded-md pb-4 drop-shadow-lg">
       <GroceryListAccordion />
